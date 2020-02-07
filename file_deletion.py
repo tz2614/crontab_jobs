f5a11fffeedf35d1fcaabd6eb9834ab589843136
Tengyue Zheng <tengyue.zheng@mft.nhs.uk>
2020-02-07 17:27:39 +0000

    whitelist dirs included, and additional checks added, ready for production

--- file_deletion.py
+++ file_deletion.py
@@ -1,18 +1,20 @@
 import datetime
 import sys
 import os
+import argparse
+
 
 # email:tengyue.zheng@mft.nhs.uk
-# date: 2020-01-24
+# date: 2020-02-07
 
 """
-Find all files and delete them based on modified date"
+Find all bam files in whitelisted dir /mnt/cmftgen5/temp/ and delete them based on modified date"
 """
 
 """
 to run the script, execute the following:
 
-$ python create_file_list.py <tmp_dir> <file_type> <days> <output_file>
+$ python create_file_list.py <tmp_dir> <file_type> <days> <white_list_dirs><output_file>
 
 <tmp_dir>
 e.g./full/path/to/tmp_dir
@@ -23,42 +25,202 @@ e.g.".bam"
 <days>
 e.g.90
 
+<white_list_dirs>
+e.g.white_listed_dirnames.txt
+
 <output_file>
 e.g.YYYY-MM-DD<file_type>.deletion.log
 
 """
 
-def create_file_list(file_type, tmp_dir):
+temp_dir = "/mnt/cmftgen5/temp"
 
-    # The following creates a list of a particular file type in in the specified directory.
+def temp_dir_deletion_whitelist():
 
-    file_type = str(file_type)
-    tmp_dir = os.path.abspath(tmp_dir)
-    assert os.path.isabs(tmp_dir), "{} NOT absolute".format(tmp_dir)
-    assert os.path.exists(tmp_dir), "{} DO NOT exist".format(tmp_dir)
-    assert os.path.isdir(tmp_dir), "{} is NOT a directory".format(tmp_dir)
+	# whitelisted dirs to search files for deletion
+
+	directories = [
+	"APC",
+	"APC_MUTYH",
+	"BEST1_LLM",
+	"BRCA",
+	"Breast_cancer_LLM",
+	"CAH",
+	"Cancer_Pharmacogenomics",
+	"Cardiac",
+	"Cataract",
+	"CF_LLM",
+	"CORE",
+	"Exome_NGS",
+	"FE_Full",
+	"FFPE_BRCA",
+	"FFPE_Lynch",
+	"FFPE_Overgrowth",
+	"GLA",
+	"HIV",
+	"HRD",
+	"Inherited_cancer",
+	"Kabuki_syndrome",
+	"KATNA1",
+	"Lynch_syndrome",
+	"LZTR1",
+	"LZTR1_SMARCB1",
+	"Macular_dystrophy_LLM",
+	"Meningococcal",
+	"Metabolic",
+	"MiSeq",
+	"Myeloid",
+	"NF1",
+	"NF1_DNA",
+	"NF2",
+	"OCA",
+	"Ophthalmic",
+	"ORF15",
+	"Overgrowth",
+	"Progressive_joint_contractures",
+	"PTCH1",
+	"PTEN",
+	"QIAseq_BRCA",
+	"QIAseq_Core",
+	"QIAseq_HRD",
+	"QIAseq_KTP",
+	"QIAseq_RNA_fusion",
+	"RB1",
+	"RD",
+	"Research",
+	"Retinal_dystrophy",
+	"RNASeq",
+	"Schwannomatosis",
+	"Schwannomatosis_validation",
+	"Severe_Pre_eclampsia",
+	"SLD",
+	"SLE",
+	"SMARCB1",
+	"TARG",
+	"Thymoma",
+	"UrinaryTractExomes",
+	"X_linked_RP_LLM"
+	]
+
+	return directories
+
+def check_tmp_dir(tmp_dir, log_file):
+
+	# check the temp dir supplied is correct
+
+	tmp_dir = os.path.abspath(tmp_dir) + "/"
+	assert os.path.isabs(tmp_dir), "{} NOT absolute".format(tmp_dir)
+	assert os.path.exists(tmp_dir), "{} DO NOT exist".format(tmp_dir)
+	assert os.path.isdir(tmp_dir), "{} is NOT a directory".format(tmp_dir)
+	#print (tmp_dir)
+
+	if tmp_dir == "{0}".format(temp_dir):
+		print ("CORRECT tmp_dir: {0}".format(tmp_dir))
+		with open (log_file, "a") as log:
+			log.writelines("CORRECT tmp_dir: {0}\n".format(tmp_dir))
+		return tmp_dir
+	else:
+		print ("WRONG tmp_dir: {0}, check path in /mnt/cmftgen5/temp/".format(tmp_dir))
+		with open (log_file, "a") as log:
+			log.writelines("WRONG tmp_dir: {0}, check path in /mnt/cmftgen5/temp/".format(tmp_dir)+"\n")
+		sys.exit()
+
+def check_dir(dirpath, log_file):
+	
+	# check each dir path exist, and is a dir
+
+	dirpath = os.path.abspath(dirpath) + "/"
+	try:
+		assert os.path.isabs(dirpath), "{} NOT absolute".format(dirpath)
+	except AssertionError as a:
+		with open (log_file, "a") as log:
+			log.writelines("{0}".format(a)+"\n")
+		sys.exit()
+	
+	try:
+		assert os.path.exists(dirpath), "{} DO NOT exist".format(dirpath)
+	except AssertionError as e:
+		with open (log_file, "a") as log:
+			log.writelines("{0}".format(e)+"\n")
+		sys.exit()
+	
+	try:
+		assert os.path.isdir(dirpath), "{} is NOT a directory".format(dirpath)
+	except AssertionError as d:
+		with open (log_file, "a") as log:
+			log.writelines("{0}".format(d)+"\n")
+		sys.exit()
+
+	if dirpath:
+		print ("CORRECT dirpath: {0}".format(dirpath))
+		with open (log_file, "a") as log:
+			log.writelines("CORRECT dirpath: {0}\n".format(dirpath))
+		return dirpath	
+	
+	else:
+		print ("WRONG dirpath: {0}, check path in /mnt/cmftgen5/temp/".format(dirpath))
+		with open (log_file, "a") as log:
+			log.writelines("WRONG dirpath: {0}, check path in /mnt/cmftgen5/temp/".format(dirpath)+"\n")
+		sys.exit()
+
+def create_file_list(file_types, tmp_dir, log_file):
+
+    # The following creates a list of a BAM and BAM.BAI in the specified directory.
 
     file_list = []
-    
-    for root, dirname, filenames in os.walk(tmp_dir):
-        for filename in filenames:
-            #find all path to files
-            if filename.endswith(file_type):
-                file_list.append(os.path.join(root, filename))
-            
-    sorted_file_list = sorted(file_list)
-    print ("list of files in {0} generated".format(tmp_dir))
-    print ("file type: {0}".format(file_type))
-    
-    return sorted_file_list
 
-def check_file_date(sorted_file_list, days):
+    print ("file types: {0}".format(file_types))
+    print ("generate a list of files with above file types")
+
+    for file_type in file_types:
+
+	    for root, dirname, filenames in os.walk(tmp_dir):
+	        for filename in filenames:
+	            #find all path to files
+	            if filename.endswith(file_type):
+	                file_list.append(os.path.join(root, filename))
+	            
+    print ("list of files in {0} generated".format(tmp_dir))
+    with open(log_file, "a") as log:
+    	log.writelines("file types: {0}\n".format(file_types))
+    	log.writelines("list of files in {0} generated\n".format(tmp_dir))
+    
+    return file_list
+
+def check_files_against_whitelist(sorted_file_list, tmp_dir, log_file):
+
+	# check the list of file paths generated against whitelist dirs
+
+	whitelist = ["{0}{1}".format(tmp_dir, str(x)) for x in temp_dir_deletion_whitelist()]	
+
+	# check whitelist dirs exists
+
+	for dirs in whitelist:
+		if check_dir(dirs, log_file):
+			continue
+		else:
+			sys.exit()
+
+	new_filtered_file_list = []
+	
+	print ("Checking file paths against whitelist")
+	with open (log_file, "a") as log:
+		log.writelines("Checking file paths against whitelist\n")
+
+	for folder in whitelist:
+		[new_filtered_file_list.append(f) for f in sorted_file_list if folder in f]
+	
+	sorted_unique_file_list = sorted(set(new_filtered_file_list))
+
+	return sorted_unique_file_list
+
+def check_file_date(sorted_file_list,expiry_period, log_file, file_types):
 
 	# check file date of files in tmp_ir, parse file paths to 4 lists based on modified date
 
 	today = datetime.datetime.today()
-	print ("Date of check:{}".format(today))
-	days = int(days)
+	print ("Time of check:{}".format(today))
+	days = int(expiry_period)
 	
 	over_duration = []
 	under_duration = []
@@ -74,11 +236,10 @@ def check_file_date(sorted_file_list, days):
 		#print (duration)
 	
 		if duration.days > days:
-
 			#WHEN READY UNCOMMENT THE FOLLOWING TO START TESTING DELETION
-			os.remove(f)
+			#os.remove(f)
 			print ("over_duration: {0} {1} {2}".format(modified_date, f, "deleted"))
-			over_duration.append("over_duration: {0} {1} {2}".format(modified_date, f, "deleted"))
+			over_duration.append("over_duration(deleted): {0} {1}".format(modified_date, f))
 
 			#WHEN READY COMMENT THE FOLLOWING
 			#print ("over_duration: {0} {1}".format(modified_date, f))
@@ -92,39 +253,81 @@ def check_file_date(sorted_file_list, days):
 			equal_duration.append("equal_duration:{0}".format(days))
 		else:
 			#print ("edge_case: {0} {1}".format(modified_date, f))
-			edge_case.append("equal_duration:{0}".format(days))
+			edge_case.append("unknown:{0}".format(days))
 
 	if not over_duration:
-		print ("All files less than 90 days old")
+		print ("All files < 90 days old")
+		with open(log_file, "a") as log:
+			log.writelines("All files < 90 days old")
+			sys,exit()
+	else:
+		log_file = write_to_output(over_duration, log_file)
+		log_file = write_to_output(equal_duration, log_file)
+		log_file = write_to_output(under_duration, log_file)
+		log_file = write_to_output(edge_case, log_file)
 
-	print ("check COMPLETE")
+	remain_list = equal_duration + under_duration + edge_case
 
-	return over_duration, under_duration, equal_duration, edge_case
+	for ft in file_types:
+		ft_delete_list = [f for f in over_duration if f.endswith(ft)]
+		ft_remain_list = [f for f in remain_list if f.endswith(ft)]
+		print ("number of {0} files deleted: {1}".format(ft, len(ft_delete_list)))
+		print ("number of {0} files remain: {1}".format(ft, len(ft_remain_list)))
+		with open (log_file, "a") as log:
+			log.writelines("number of {0} files deleted: {1}\n".format(ft, len(ft_delete_list)))
+			log.writelines("number of {0} files remain: {1}\n".format(ft, len(ft_remain_list)))
 
+	print ("CHECK COMPLETE")
+	print ("LOG for modified dates and file paths created: {}".format(log_file))
+	
+	with open(log_file, "a") as log:
+		log.writelines("number of files in total deleted: {0}\n".format(len(over_duration)))
+		log.writelines("number of files in total remain: {0}\n".format(len(remain_list)))
+		log.writelines("CHECK COMPLETE\n")
+		log.writelines("LOG for modified dates and file paths created: {}\n".format(log_file))
 
-def write_to_output(over_d, under_d, equal_d, edge_c, output_file):
+	return log_file
+
+def write_to_output(file_list, log_file):
 
 	# Record the files checked, deleted in a log file
 
-    output_file = str(output_file)
+    for date_file in file_list:
+    	if not date_file:
+    		continue
+    	else:
+    		with open (log_file, "a") as log:
+        		log.writelines(date_file + "\n")
 
-    sorted_file_list = [over_d, equal_d, edge_c, under_d]
+    return log_file
 
-    for due_file_list in sorted_file_list:
-    	with open (output_file, "a") as fh:
-        	for date_file in due_file_list:
-        		fh.write(date_file+"\n")
+def parse_arguments():
 
-    print ("LOG for modified dates and file paths created: {}".format(output_file))
-    return output_file
+	parser = argparse.ArgumentParser("script to parse certain file types from a temp_dir, and delete them if over expiry date")
 
+	parser.add_argument("dir", nargs="?", help="temp directory where the files are located")
+	parser.add_argument("expiry_period", nargs="?", help="amount of time (days), files are to be kept in the temp directory before deletion")
+	parser.add_argument("log", nargs="?", help="log file of what files are deleted, and what files are kept")
+	parser.add_argument('-f', '--file_types', nargs='+', required=True, help="file_types to search for deletion")
 
-def main(tmp_dir, file_type, days, output_file):
+	args = parser.parse_args()
 
-    sorted_file_list = create_file_list(file_type, tmp_dir)
-    over_d, under_d, equal_d, edge_c = check_file_date(sorted_file_list, days)
-    output = write_to_output(over_d, under_d, equal_d, edge_c, output_file)
+	tmp_dir = args.dir
+	expiry_period = args.expiry_period
+	log_file = args.log
+	file_types = args.file_types
+	#print (file_types)
+
+	return tmp_dir, expiry_period, log_file, file_types
+
+def main(tmp_dir, expiry_period, log_file, file_types):
+
+	tmp_dir = check_tmp_dir(tmp_dir, log_file)
+	file_list = create_file_list(file_types, tmp_dir, log_file)
+	sorted_file_list = check_files_against_whitelist(file_list, tmp_dir, log_file)
+	log_file = check_file_date(sorted_file_list, expiry_period, log_file, file_types)
 
 if __name__ == "__main__":
-    main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+    tmp_dir, expiry_period, log_file, file_types = parse_arguments()
+    main(tmp_dir, expiry_period, log_file, file_types)
 
--- temp_dir_bam_bai_deletion.sh
+++ temp_dir_bam_bai_deletion.sh
@@ -7,24 +7,24 @@ module add apps/python/2.7.8/gcc-4.8.5
 ft1=".bam"
 ft2=".bai"
 ft3=".log"
+spacing="_"
 date_time=$(date -I)
 
-script_path="/users/tz1/bin/crontab/file_deletion.py"
-# WHEN TESTING change the following dir
-# tmp_dir="/mnt/cmftgen5/temp/"
+script_path="/users/tz1/git/crontab_jobs/file_deletion.py"
+# WHEN TESTING change the following dir to test dir
+tmp_dir="/mnt/cmftgen5/temp/"
 
 # test dir containing dummy data
-tmp_dir="/mnt/repository/Bioinformatics/tengyue_zheng_projects/crontab/test_data/"
+#tmp_dir="/mnt/repository/Bioinformatics/tengyue_zheng_projects/crontab/test_data/"
 
 duration="90"
 action=".deletion"
 
-log_fp1=$tmp_dir$date_time$ft1$action$ft3
-log_fp2=$tmp_dir$date_time$ft1$ft2$action$ft3
+log_fp=$tmp_dir$date_time$ft1$spacing$ft1$ft2$spacing$ft2$spacing$action$ft3
+#echo $log_fp
 
 #echo $log_fp1
 #echo $log_fp2
 
 # WHEN READY UNCOMMENT THE  FOLLOWING TO START EXECUTING THE PYTHON SCRIPT
-python $script_path $tmp_dir $ft1 $duration $log_fp1
-python $script_path $tmp_dir $ft1$ft2 $duration $log_fp2
+python $script_path $tmp_dir $duration $log_fp -f $ft1 $ft1$ft2 $ft2 
